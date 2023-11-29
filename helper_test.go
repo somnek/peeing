413e8a2f635a48f5611473b519cfd571d642bea8
somnek <mumscarpetiswet@gmail.com>
2023-11-29 14:48:29 +0800

    test for ocnvert to block unit

--- helper_test.go
+++ helper_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"testing"
+	"time"
+)
 
 func TestIsisValidInput(t *testing.T) {
 	tests := []struct {
@@ -22,5 +25,28 @@ func TestIsisValidInput(t *testing.T) {
 				t.Errorf("isValidInput(%v) = %v, want %v", tt.input, got, tt.want)
 			}
 		})
+	}
+}
+
+func TestConvertToBlockUnit(t *testing.T) {
+	tests := []struct {
+		name  string
+		input time.Duration
+		want  rune
+	}{
+		{"minimum", 0 * time.Millisecond, BARS[0]},
+		{"mid", 200 * time.Millisecond, BARS[3]},
+		{"max", 400 * time.Millisecond, BARS[7]},
+		{"exceed max", 500 * time.Millisecond, BARS[7]},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := convertToBlockUnit(tt.input)
+			if got != tt.want {
+				t.Errorf("convertToBlockUnit(%v) = %v, want %v", tt.input, got, tt.want)
+			}
+		})
+
 	}
 }
